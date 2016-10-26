d744b567dc6e129c85e9e962cbf56a7ce0e4e4c3
Eugenio Barquin <eu.barquin@me.com>
2016-10-27 00:06:49 +0200

    Realizado el POST de usuarios

--- nodepop/app.js
+++ nodepop/app.js
@@ -39,7 +39,7 @@ app.use('/', require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
 //app.use('/apiv1/anuncios', require('./routes/apiv1/anuncios'));
-app.use('apiv1/usuarios', require('./routes/apiv1/usuarios'));
+app.use('/apiv1/usuarios', require('./routes/apiv1/usuarios'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
