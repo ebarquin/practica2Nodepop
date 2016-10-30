193b70d566be2679d9dab4c95db3b2eba3f10f91
Eugenio Barquin <eu.barquin@me.com>
2016-10-30 12:05:15 +0100

    Incluyo los usuarios en el installDB.js

--- nodepop/scripts/installDB.js
+++ nodepop/scripts/installDB.js
@@ -12,14 +12,25 @@ mongoClient.connect(url, function(err, db) {
         return console.log('Error: ', err);
     }
 
-    //Elimino las tablas de la base de datos
+    //Elimino la tabla de anuncios de la  base de datos
     db.collection('anuncios').deleteMany({}, function(err) {
 
-        if (err) {
-            return console.log('Error', err);
+        if(err){
+            console.log('Error', err);
+            return;
         }
-
         console.log('Anuncios Borrados');
+
+    });
+
+    //Elimino la tabla de usuarios de la base de datos
+    db.collection('usuarios').deleteMany({}, function(err){
+
+        if(err){
+            console.log('Error', err);
+            return;
+        }
+        console.log('Usuarios Borrados');
 
     });
 
@@ -27,13 +38,27 @@ mongoClient.connect(url, function(err, db) {
     //Cargo los anuncios del archivo anuncios.json
     db.collection('anuncios').insertMany(json.anuncios, function(err, registros) {
 
-        if (err) {
-            return console.log('Error', err);
+        if(err){
+            console.log('Error', err);
+            return;
         }
 
         console.log('Anuncios Agregados ', registros.ops);
 
     });
+
+    //Cargo los anuncios del archivo anuncios.json
+    db.collection('usuarios').insertMany(json.usuarios, function(err, registros) {
+
+        if(err){
+            console.log('Error', err);
+            return;
+        }
+
+        console.log('Usuarios Agregados ', registros.ops);
+
+    });
+
 
 
 });
