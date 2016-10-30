6961c34ffc0d615774f878ea37c05a65edde5a80
Eugenio Barquin <eu.barquin@me.com>
2016-10-30 19:29:47 +0100

    Se incluyen los filtros

--- nodepop/routes/apiv1/anuncios.js
+++ nodepop/routes/apiv1/anuncios.js
@@ -11,23 +11,46 @@ var Anuncio = mongoose.model('Anuncio');
 //Peticiones get
 
 router.get('/', function (req, res, next) {
+    //parametros de la query string
+    var tags = req.query.tags;
     var venta = req.query.venta;
+    var nombre = req.query.nombre;
+    var limit = parseInt(req.query.limit)|| null;
     var sort = req.query.sort || null;
 
+
+
     var filter = {};
+
+    //comprobamos los datos de la query que han introducido
+
+    if (typeof tags === 'string'){
+        filter.tags = tags;
+    }
 
     if (typeof venta !== 'undefined'){
         filter.venta = venta;
     }
 
+    if (nombre){
+        filter.nombre = new RegExp('^' + req.query.nombre, "i");
+    }
 
-    Anuncio.find(filter).sort(sort).where('precio').gte(10).lte(50).exec(function(err, anuncios){
+
+
+
+
+
+
+    Anuncio.find(filter).limit(limit).sort(sort).exec(function(err, anuncios){
        if (err){
            next(err);
            return;
        }
        res.json({success:true , anuncios: anuncios});
     });
+
+
 });
 
 
--- nodepop/routes/apiv1/usuarios.js
+++ nodepop/routes/apiv1/usuarios.js
@@ -5,12 +5,45 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Usuario = mongoose.model('Usuario');
+var jwt = require('jsonwebtoken');
 
+/*
+//Creamos un nuevo login
+router.post('/login', function (req, res, next){
+    var email = req.body.user;
+    var pass = req.body.pass;
+
+  //Localizamos al usuario que tiene ese email
+
+    Usuario.findOne(email).exec(function(err, usuario){
+        if (err){
+            res.json({success:false, error:"No está en la base de datos"});
+
+        }
+
+        res.json({success:true , anuncios: usuario});
+    });
+
+    if(usuario.clave!= pass){
+        res.json({success: false, error:"Password Incorrecto"});
+
+    }
+
+    var token = jwt.sign({id : usuario._id}, '123456', {
+        expiresIn:'2 days'
+    });
+
+    res.json({success:true, token:token});
+
+
+});
+
+*/
 //Podemos crear nuevos usuarios mediante un método post
 
 router.post('/', function(req, res){
 
-    var usuario = new Usuario(req.body);
+    var usuario = new usuario(req.body);
 
     usuario.save(function(err, usuarioGuardado){
         if (err){
@@ -21,6 +54,9 @@ router.post('/', function(req, res){
     });
 
 });
+
+
+
 
 
 
